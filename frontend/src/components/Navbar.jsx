7c1f08e966be7427db1d80bab3d73e612888da8d
Aravindkammari <aravind.kammari07@gmail.com>
2025-11-30 19:23:52 +0530

    navbar created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../assets/images/logo.png";
+import cart from "../assets/images/cart.png";
+
+const Navbar = () => {
+  return (
+    <div className="flex gap-180 m-3 px-6 font-[Outfit,sans-serif]">
+      <div className="flex items-center">
+        <img className="w-[100px] h-[78px]" src={logo} alt="" />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex gap-10 items-center justify-center ">
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About Us</li>
+          <li>FAQ</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+          <li>
+            <img src={cart} alt="" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
